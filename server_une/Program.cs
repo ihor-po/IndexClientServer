5e483613f125a3250369bdca604e696922630d52
Ihor Porotikov <sobikashi@gmail.com>
2018-10-17 22:38:14 +0300

    server can run

--- server_une/Program.cs
+++ server_une/Program.cs
@@ -5,53 +5,21 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using server_une;
 
 namespace server_une
 {
     class Program
     {
-
-
-
         static void Main(string[] args)
         {
-            //ip = IPAddress.Parse("192.168.88.217");
-            //ep = new IPEndPoint(ip, 12088);
-            Server();
+            Server ser = new Server("192.168.88.217", 12088);
+
+            ser.ServerStart();
+            Console.ReadKey();
+            ser.ServerStop();
+            
         }
-
-        static void Server()
-        {
-
-
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
-
-            //socket.Bind(ep);
-            //socket.Listen(10); //перевод сокета в  состояние прослушивания с очередью подключения 10
-
-            try
-            {
-                while (true)
-                {
-                    
-                    tmpSocket.Send(System.Text.Encoding.ASCII.GetBytes("Connection success...\r\n"));
-                    Console.WriteLine(tmpSocket.RemoteEndPoint.ToString());
-                    string res = GetStreetsByIndex("50000");
-                    tmpSocket.Send(System.Text.Encoding.ASCII.GetBytes(res));
-                    tmpSocket.Send(System.Text.Encoding.ASCII.GetBytes("\r\nClose connection\r\n"));
-                    socketClose(tmpSocket);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                socketClose(socket);
-            }
-        }
-
 
         #region CLIENT
         static void Client()
@@ -87,52 +55,10 @@ namespace server_une
             }
             finally
             {
-                socketClose(s);
+                //socketClose(s);
             }
         }
         #endregion
 
-        private static void socketClose(Socket s)
-        {
-            s?.Shutdown(SocketShutdown.Both);
-            s?.Close();
-        }
-
-        /// <summary>
-        ///  Search streets by index
-        /// </summary>
-        /// <param name="index"></param>
-        /// <returns></returns>
-        private static string GetStreetsByIndex(string index)
-        {
-            StringBuilder res = new StringBuilder("", 255);
-
-            try
-            {
-                using (Address_dbContainer data = new Address_dbContainer())
-                {
-                    var streets = data.Address.Where(item => item.PostIndex.PostI == index);
-
-                    if (streets != null)
-                    {
-
-                        foreach (Address item in streets)
-                        {
-                            res.AppendFormat(item.LocalAddress + ";");
-                        }
-                    }
-                    else
-                    {
-                        res.Append("По заданному индексу улицы не найдены!");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                res.Append(ex.Message);
-            }
-
-            return res.ToString();
-        }
     }
 }
--- server_une/Server.cs
+++ server_une/Server.cs
@@ -12,7 +12,6 @@ namespace server_une
     {
         delegate void DelegateConnect(Socket _socket);
         delegate void DelegateNetwork(Socket _socket);
-//        private IPAddress ip;
         private IPEndPoint ep;
         private Socket socket;
 
@@ -42,26 +41,105 @@ namespace server_une
             dn.BeginInvoke(socket, null, null);
         }
 
+        /// <summary>
+        /// Close server connection
+        /// </summary>
+        public void ServerStop()
+        {
+            CloseConnection(socket);
+        }
+
+        /// <summary>
+        /// Server recive mode
+        /// </summary>
+        /// <param name="_socket"></param>
         private void ReciveMode(Socket _socket)
         {
-            
             int l;
             while (true)
             {
-                Socket tmpSocket = socket.Accept();
-                byte[] buffer = new byte[1024];
-                l = tmpSocket.Receive(buffer);
+                try
+                {
+                    Socket tmpSocket = _socket.Accept();
+                    byte[] buffer = new byte[1024];
+                    l = tmpSocket.Receive(buffer);
 
-                index = Encoding.ASCII.GetString(buffer, 0, l); //получение индекса от клиента
+                    index = Encoding.ASCII.GetString(buffer, 0, l); //получение индекса от клиента
 
+                    //Вывод в консоль данные о подключенном клиенте
+                    IPEndPoint remoteIpEndPoint = tmpSocket.RemoteEndPoint as IPEndPoint;
+                    if (remoteIpEndPoint != null)
+                    {
+                        Console.WriteLine("Connection from " + remoteIpEndPoint.Address + ":" + remoteIpEndPoint.Port);
+                        Console.WriteLine("Resive data >>> " + index);
+                    }
+                    else
+                    {
+                        Console.WriteLine(tmpSocket.RemoteEndPoint.ToString() + ">>>" + index);
+                    }
 
-                
+                    DelegateConnect dc = new DelegateConnect(SendMode);
+                    dc.BeginInvoke(tmpSocket, null, null);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-            do
+        }
+
+        private void SendMode(Socket _socket)
+        {
+            string result = GetStreetsByIndex(index);
+            _socket.Send(Encoding.ASCII.GetBytes(result));
+            CloseConnection(_socket);
+        }
+
+        /// <summary>
+        ///  Search streets by index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private static string GetStreetsByIndex(string _index)
+        {
+            StringBuilder res = new StringBuilder("", 255);
+
+            try
             {
-                l = s.Receive(buffer);
-                Console.WriteLine(System.Text.Encoding.ASCII.GetString(buffer, 0, l));
-            } while (l > 0);
+                using (Address_dbContainer data = new Address_dbContainer())
+                {
+                    var streets = data.Address.Where(item => item.PostIndex.PostI == _index);
+
+                    if (streets != null)
+                    {
+
+                        foreach (Address item in streets)
+                        {
+                            res.AppendFormat(item.LocalAddress + ";");
+                        }
+                    }
+                    else
+                    {
+                        res.Append("По заданному индексу улицы не найдены!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                res.Append(ex.Message);
+            }
+
+            return res.ToString();
+        }
+
+        /// <summary>
+        /// Close connection
+        /// </summary>
+        /// <param name="s"></param>
+        private void CloseConnection(Socket s)
+        {
+            s?.Shutdown(SocketShutdown.Both);
+            s?.Close();
         }
     }
 }
